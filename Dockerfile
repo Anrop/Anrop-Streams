a221efb8995d34ec8d88041ed8bb2c5c0666ba08
Jesper Falk <jesper@loffa.se>
2019-07-24 10:40:53 +0200

    Change dependency management to go mod (#18)

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,11 @@
 FROM golang:1.12-alpine AS builder
 RUN apk --no-cache add git
-RUN go get github.com/constabulary/gb/...
 WORKDIR /build/
-COPY src/ /build/src/
-COPY vendor/ /build/vendor/
-RUN gb build all
+COPY . /build/
+RUN go build
 
 FROM alpine:latest
 RUN apk --no-cache add ca-certificates
 WORKDIR /app/
-COPY --from=builder /build/bin/streams /app/streams
+COPY --from=builder /build/Anrop-Streams /app/streams
 CMD /app/streams
---
+++ main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/Anrop/Anrop-Streams/api"
+	"github.com/Anrop/Anrop-Streams/db"
+	"github.com/Anrop/Anrop-Streams/twitch"
+	"github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
+	_ "github.com/joho/godotenv/autoload"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+	databaseURL := os.Getenv("DATABASE_URL")
+	newRelicLicenseKey := os.Getenv("NEW_RELIC_LICENSE_KEY")
+	twitchClientID := os.Getenv("TWITCH_CLIENT_ID")
+
+	if port == "" {
+		port = "8080"
+	}
+
+	var err error
+
+	err = db.Connect(databaseURL)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error connecting to database: %q\n", err)
+		os.Exit(1)
+	}
+
+	err = twitch.CreateSession(twitchClientID)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error initializing Twitch Session: %q\n", err)
+		os.Exit(1)
+	}
+
+	r := mux.NewRouter()
+	r.HandleFunc("/", api.StreamsHandler)
+
+	if newRelicLicenseKey != "" {
+		api.SetupNewRelic(newRelicLicenseKey)
+		r = api.InstrumentRoutes(r)
+	}
+
+	var handler http.Handler
+	handler = handlers.CORS()(r)
+	handler = handlers.CompressHandler(handler)
+
+	fmt.Fprintf(os.Stdout, "Server launching on port %s\n", port)
+
+	// Bind to a port and pass our router in
+	http.ListenAndServe(":"+port, handler)
+}
