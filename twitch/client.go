085703bbb75d6e2f7ae44426de2bd48da783e0c0
Björn Dahlgren <bjorn@dahlgren.at>
2020-05-16 22:03:27 +0200

    Add Twitch authentication using app token

--- twitch/client.go
+++ twitch/client.go
@@ -2,18 +2,37 @@ package twitch
 
 import (
 	"github.com/nicklaw5/helix"
+	"time"
 )
 
 var (
 	// Client holds the client for Twitch API access
-	Client *helix.Client
+	Client          *helix.Client
+	token           *helix.AppAccessCredentials
+	tokenExpiryTime time.Time
 )
 
 // CreateSession creates a new session to the Twitch API
-func CreateSession(clientID string) error {
+func CreateSession(clientID string, clientSecret string) error {
 	var err error
 	Client, err = helix.NewClient(&helix.Options{
-		ClientID: clientID,
+		ClientID:     clientID,
+		ClientSecret: clientSecret,
 	})
 	return err
+}
+
+// UpdateAppToken updates client authorization with a new app token when needed
+func UpdateAppToken() error {
+	if token == nil || time.Now().After(tokenExpiryTime) {
+		tokenRequestTime := time.Now()
+		res, err := Client.GetAppAccessToken()
+		if err != nil {
+			return err
+		}
+		token = &res.Data
+		tokenExpiryTime = tokenRequestTime.Add(time.Duration(token.ExpiresIn) * time.Second)
+		Client.SetAppAccessToken(token.AccessToken)
+	}
+	return nil
 }
