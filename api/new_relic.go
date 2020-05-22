aeaa42d2ef2d567a283f5e58ec718b5bc2c9e566
Björn Dahlgren <bjorn@dahlgren.at>
2020-05-22 17:14:42 +0200

    Migrate from deprecated new relic instrumentation (#24)

--- api/new_relic.go
+++ api/new_relic.go
@@ -5,35 +5,23 @@ import (
 	"os"
 
 	"github.com/gorilla/mux"
-	newrelicGorilla "github.com/newrelic/go-agent/v3/integrations/nrgorilla"
-	newrelic "github.com/newrelic/go-agent/v3/newrelic"
+	"github.com/newrelic/go-agent/v3/integrations/nrgorilla"
+	"github.com/newrelic/go-agent/v3/newrelic"
 )
 
 const appName = "Streams"
 
-var (
-	newRelicApp *newrelic.Application
-)
-
-// SetupNewRelic performs initial configuration
-func SetupNewRelic(licenseKey string) {
+// SetupNewRelic sets up monitoring in NewRelic
+func SetupNewRelic(licenseKey string, r *mux.Router) {
 	app, err := newrelic.NewApplication(
 		newrelic.ConfigAppName(appName),
 		newrelic.ConfigLicense(licenseKey),
 	)
-	newRelicApp = app
 
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Error starting New Relic: %q", err)
 		os.Exit(1)
 	}
-}
 
-// InstrumentRoutes adds NewRelic routes to mux.Router
-func InstrumentRoutes(r *mux.Router) *mux.Router {
-	if newRelicApp != nil {
-		return newrelicGorilla.InstrumentRoutes(r, newRelicApp)
-	}
-
-	return r
+	r.Use(nrgorilla.Middleware(app))
 }
